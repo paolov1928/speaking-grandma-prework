daf8362328fe8add89a70caaf4d54b1119d8e5cc
Paolo Ventura <ventura.paolo@gmail.com>
2018-10-04 20:43:19 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,12 +2,12 @@
 
 def speak_to_grandma(speech)
   if speech == speech.upcase
-    puts 'NO, NOT SINCE 1938!'
+    return 'NO, NOT SINCE 1938!'
   elsif
     speech == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
+    return 'I LOVE YOU TOO PUMPKIN!'
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
